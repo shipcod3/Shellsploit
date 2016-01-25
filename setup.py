98520f0f8eafc0d2f045e1b84de35b7b5e696ed5
b3mb4m <b3mb4m@tuta.io>
2016-01-25 16:02:44 +0200

    Uninstall update

--- setup.py
+++ setup.py
@@ -1,17 +1,34 @@
-from setuptools import setup,find_packages
+from optparse import OptionParser
+from os import system
+from os import path
+from os import unlink
+from sys import exit
 
 
-setup(
-    name = "cmdline-shellsploit",
-    packages = find_packages(),
-    entry_points={
-        'console_scripts': [
-            'shellsploit = shell.shellsploit:main',
-        ]
-    },
-    version = "0.1",
-    description = ("An open source cuztomize shellcode,backdoor,injector generator."),
-    author = "B3mB4m",
-    author_email = "b3mb4m@protonmail.com",
-    url='https://github.com/b3mb4m/Shellsploit',
-    )
+
+if __name__ == '__main__':
+	parser = OptionParser()
+	parser.add_option('-s', '--setup', action="store")
+	options, args = parser.parse_args()
+
+	if not options.setup:
+		exit("\n\tUsage : setup.py -s/--setup install or uninstall ..\n")
+	else:
+		if options.setup == 'install':
+			system('python easyinstall.py install --record shellsploit.ini')
+		elif options.setup == 'uninstall': 
+			if not path.isfile('shellsploit.ini'):
+				exit('\n\tIf you want uninstall you must install it first.\n')
+			else:
+				files = open('shellsploit.ini', 'r').readlines()
+				unlink('shellsploit.ini')
+				print ('')
+				for x in files:
+					if path.isfile(x.strip()):
+						print ('Remove : {0}').format(x.strip()) 
+						unlink(x.strip())
+				else:
+					exit('\nUninstall complate ! \n')
+
+		else:
+			exit("\n\tUsage : setup.py -s/--setup install or uninstall ..\n")
